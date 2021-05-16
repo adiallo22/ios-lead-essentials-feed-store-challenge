3b0b1e0ed62916ef4e918bc148b5c46bccecb950
Abdul <rahimi.diallo96@gmail.com>
2021-05-16 16:33:55 -0400

    add neccessary entities in FeedStore xcdatamodel. perform an insertion and deletion

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,14 +29,65 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let localContext = context
+		localContext.perform {
+			let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+			request.returnsObjectsAsFaults = false
+			if let cache = try! localContext.fetch(request).first {
+				let localFeed = cache.feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+				completion(.found(feed: localFeed, timestamp: cache.timestamp))
+			} else {
+				completion(.empty)
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		let localContext = context
+		localContext.perform {
+			let cache = ManagedCache(context: localContext)
+			cache.feed = ManagedFeedImage.images(from: feed, in: localContext)
+			cache.timestamp = timestamp
+			try! localContext.save()
+			completion(nil)
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
+	}
+}
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+
+	static func images(from localFeed: [LocalFeedImage],
+	                   in context: NSManagedObjectContext) -> NSOrderedSet {
+		NSOrderedSet(array: localFeed.map { local in
+			let managed = ManagedFeedImage(context: context)
+			managed.id = local.id
+			managed.imageDescription = local.description
+			managed.location = local.location
+			managed.url = local.url
+			return managed
+		})
+	}
+
+	var local: LocalFeedImage {
+		LocalFeedImage(id: id,
+		               description: imageDescription,
+		               location: location,
+		               url: url)
 	}
 }
